301633271d322e2a14e18c02757e57786960cd8f
bunnie <bunnie@kosagi.com>
2024-02-08 14:39:12 +0800

    add xous target details

--- src/lib.rs
+++ src/lib.rs
@@ -150,7 +150,7 @@
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/getrandom/0.2.6"
+    html_root_url = "https://docs.rs/getrandom/0.2.11"
 )]
 #![no_std]
 #![warn(rust_2018_idioms, unused_lifetimes, missing_docs)]
@@ -230,7 +230,7 @@ cfg_if! {
         // uses Horizon OS (it is aarch64).
         mod util_libc;
         #[path = "3ds.rs"] mod imp;
-    } else if #[cfg(targot_os = "xous")] {
+    } else if #[cfg(target_os = "xous")] {
         #[path = "xous.rs"] mod imp;
     } else if #[cfg(feature = "custom")] {
         use custom as imp;
